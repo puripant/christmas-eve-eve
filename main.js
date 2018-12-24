d89a6241bfe127732925023179dd112a8c4e6784
Puripant <puripant@gmail.com>
2018-12-25 00:31:53 +0700

    Fix the function to find the integer difference between two time points

--- main.js
+++ main.js
@@ -2,14 +2,15 @@
 const timezone = -(new Date().getTimezoneOffset()); //in minutes
 
 const now = Date.now(); // + timezone * 60 * 1000;
-const christmas = new Date('12/25/' + (new Date()).getUTCFullYear()).getTime(); // + timezone * 60 * 1000;
+const christmas = new Date('12/25/' + (new Date()).getUTCFullYear()).getTime(); //+ timezone * 60 * 1000;
 
 function diffDays(fromMs, toMs) {
   return (toMs - fromMs) / 1000 / 60 / 60 / 24;
 }
 
 let eve = document.querySelector('#eve');
-const eveNum = Math.ceil(((diffDays(now, christmas) % 365) + 365) % 365);
+const eveNum = ((Math.ceil(diffDays(now, christmas)) % 365) + 365) % 365;
+console.log(diffDays(now, christmas));
 for (let i = 0; i < eveNum; i++) {
   if (i % 2 === 0) {
     eve.innerHTML += ' <span class="green">Eve</span>';
